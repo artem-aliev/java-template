9ce61b46c70e54c130b238fa3554e5e0209ec0cd
Natalia <zhukova2000natasha@gmail.com>
2018-09-10 20:59:20 +0300

    New sort added.

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -9,10 +9,29 @@ import java.util.List;
  */
 public class IntSort {
   public static void sort (int array[]) {
-    Arrays.sort(array);
+    myquick(array,0, array.length-1);
   }
 
   public static void sort (List<Integer> list) {
     Collections.sort(list);
+  }
+  public  static void myquick(int array[], int l, int r){
+    int i = l, j = r;
+    int t;
+    int king = array[((l + r) / 2)];
+    while (i <= j) {
+      while (array[i] < king) i++;
+      while (array[j] > king) j--;
+      if (i <= j) {
+        t = array[i];
+        array[i] = array[j];
+        array[j] = t;
+        i++; j--;
+      }
+    }
+    if (l < j)
+      myquick(array, l, j);
+    if (i < r)
+      myquick(array, i, r);
   }
 }
