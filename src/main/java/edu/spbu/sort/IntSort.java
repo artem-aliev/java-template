ba4298cd6464afc2c6226bd9a9f2eb8b80d9f8f4
rumylera <45111115+rumylera@users.noreply.github.com>
2019-09-12 01:30:02 +0300

    Update IntSort.java

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,8 +8,35 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
+  public static void swap (int x1, int x2)
+  {
+    int temp = x1;
+    x1 = x2;
+    x2 = temp;
+  }
+
+  public static void sort (int array[])
+  {
+    int length = array.length;
+    int gap = length;
+    boolean check = true;
+    while(gap>1||check)
+    {
+      if(gap>1)
+      {
+        gap=(int)(gap/1.3);
+      }
+      check = false;
+      for(int i=0; i<length-gap;i++)
+      {
+        if(array[i]>array[i+gap])
+        {
+          swap(array[i], array[i+gap]);
+          check = true;
+        }
+      }
+    }
+
   }
 
   public static void sort (List<Integer> list) {
