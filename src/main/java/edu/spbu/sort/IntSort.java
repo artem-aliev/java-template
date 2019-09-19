097b97f9302cf1251fb2bf2ea86e4d61c715a7d1
rumylera <45111115+rumylera@users.noreply.github.com>
2019-09-19 03:23:40 +0300

    qsort -> combsort

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -17,41 +17,30 @@ public class IntSort {
 
   public static void sort (int array[])
   {
-    int length = array.length;
-    int start = 0;
-    int end = length - 1;
-    qsort(start, end, array);
-  }
-  public static void qsort(int start, int end, int array[])
-  {
-      if (start >= end) {
-          return;
-      }
-      int i = start;
-      int j = end;
-      int mid = i - (i - j) / 2;
-      while (i < j) {
-          while (i < mid && (array[i] <= array[mid])) {
-              i++;
+      int length = array.length;
+      int gap = length;
+      boolean check = true;
+      while(gap>1||check)
+      {
+          if(gap>1)
+          {
+              gap=(int)(gap/1.3);
           }
-          while (j > mid && (array[mid] <= array[j])) {
-              j--;
-          }
-          if (i < j) {
-              swap(i,j,array);
-              if (i == mid) {
-                  mid = j;
-              }
-              else if (j == mid) {
-                  mid = i;
+          check = false;
+          for(int i=0; i<length-gap;i++)
+          {
+              if(array[i]>array[i+gap])
+              {
+                  swap(i, i+gap, array);
+                  check = true;
               }
           }
       }
-      qsort(start, mid, array);
-      qsort(mid+1, end, array);
+
   }
 
-  public static void sort (List<Integer> list) {
+
+    public static void sort (List<Integer> list) {
     Collections.sort(list);
   }
 }
